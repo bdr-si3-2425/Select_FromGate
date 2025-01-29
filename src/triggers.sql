d0581bf624853d4db349e8aba926a47d547102bb
Majurax <Legeekmajurax@gmail.com>
2025-01-29 23:11:28 +0100

    Rajout d'un trigger qui empêche un prolongement si le livre est réservé

--- src/triggers.sql
+++ src/triggers.sql
@@ -1,4 +1,4 @@
-CREATE TRIGGER verif_emprun
+CREATE TRIGGER verif_book_borrowed_emprun
 BEFORE INSERT ON Prets
 FOR EACH ROW
 BEGIN 
@@ -9,5 +9,20 @@ BEGIN
     ) THEN
         SIGNAL SQLSTATE '45000'
         SET MESSAGE_TEXT = "L'ouvrage est déjà emprunté sur cette période";
+    END IF;
+END;
+
+CREATE TRIGGER verif_book_reserved_prolongation
+BEFORE UPDATE ON Prets
+FOR EACH ROW
+BEGIN
+    IF EXISTS (
+        SELECT 1 FROM Reservation AS r 
+        WHERE r.id_exemplaire = NEW.id_exemplaire
+        AND r.date_expiration >= CURDATE()
+        AND NEW.date_fin >= r.date_debut
+    ) THEN 
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = "L'ouvrage est réservé";
     END IF;
 END;
