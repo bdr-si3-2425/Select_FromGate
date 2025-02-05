fa981bea27e57818bd7b0ba8559f4330a7bf9d5a
Majurax <Legeekmajurax@gmail.com>
2025-02-05 10:13:22 +0100

    Modif pour tenir compte des transferts et des bibliotheques des abonnes

--- src/Functions/End_subscription_function.sql/verif_end_of_subsription.sql
+++ src/Functions/End_subscription_function.sql/verif_end_of_subsription.sql
@@ -12,10 +12,27 @@ BEGIN
 	END IF;
 
 
+	-- Pareil avec les renouvellements
+	IF EXISTS (
+	    SELECT 1
+	    FROM Prets_Renouvellements pr
+	    JOIN Prets p ON pr.id_pret = p.id_pret
+	    WHERE p.id_abonne = NEW.id_abonne
+	    AND NEW.date_debut <= pr.date_fin
+	    AND NEW.date_fin >= pr.date_renouvellement
+	) THEN
+    	RAISE EXCEPTION 'L''abonné(e) a déjà un prêt renouvelé dans la période demandée';
+	END IF;
+
 	-- On retire les réservation en cours/futures de l'abonné(e)
 	DELETE FROM Reservations
 	WHERE id_abonne = OLD.id_personne
 	AND date_expiration >= CURRENT_DATE;
+
+	-- On retire les transferts de l'abonné(e)
+	DELETE FROM Transferts
+	WHERE id_personne = OLD.id_personne
+  	AND date_arrivee >= CURRENT_DATE;
 	
 RETURN NEW;
 END;
--- src/Tables/creation_tables.sql
+++ src/Tables/creation_tables.sql
@@ -120,6 +120,7 @@ CREATE TABLE IF NOT EXISTS Prets (
   id_pret INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   id_exemplaire INTEGER NOT NULL REFERENCES Exemplaires,
   id_abonne INTEGER NOT NULL REFERENCES Abonnes,
+  id_bibliotheque INTEGER NOT NULL REFERENCES Bibliotheques,
   date_debut DATE,
   date_fin DATE,
   retard INTEGER
@@ -145,6 +146,7 @@ CREATE TABLE IF NOT EXISTS Transferts (
   id_exemplaire INTEGER NOT NULL REFERENCES Exemplaires,
   id_bibliotheque_depart INTEGER NOT NULL REFERENCES Bibliotheques,
   id_bibliotheque_arrivee INTEGER NOT NULL REFERENCES Bibliotheques,
+  id_personne INTEGER NOT NULL REFERENCES Personnes,
   date_demande DATE NOT NULL,
   date_arrivee DATE NOT NULL
 );
